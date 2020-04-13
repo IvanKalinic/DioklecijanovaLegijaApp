5cdf2cde8f0cb3c639b0d270cc2ad2a6d56b8c17
IvanKalinic <ivankalinic1998@gmail.com>
2020-04-13 17:42:09 +0200

    Add bootstrap theme and partial view

--- DioklecijanovaLegija/DioklecijanovaLegija/App_Start/BundleConfig.cs
+++ DioklecijanovaLegija/DioklecijanovaLegija/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace DioklecijanovaLegija
 {
@@ -23,7 +22,7 @@ namespace DioklecijanovaLegija
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cosmo.css",
                       "~/Content/site.css"));
         }
     }
